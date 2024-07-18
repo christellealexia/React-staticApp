3af24c80946a53ad8b59841c766210c61fc614dc
Alexia Christelle <christellealexia53@gmail.com>
2024-07-18 21:43:57 +0200

    Used react in my app

---
+++ vite-project/src/MainContent.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Navbar } from './Navbar'
+
+export function MainContent (){
+    return  (
+    <div>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>It is interesting</li>
+        <li>It is fun</li>
+        <li>It is composable</li>
+      </ul>
+      </div>
+    )
+      
+  
+}
+     
+   
---
+++ vite-project/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export function Navbar(){
+   return (
+<header>
+   <nav>
+   <h1>React Facts</h1>
+   <h1>React course-project1</h1>
+   </nav>
+</header>
+
+  
+)
+}
+     
+     
